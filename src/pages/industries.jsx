9cfb1df05e68216c8cf9aa9dac195ca6eee353c1
mixael442 <misaelcapo720@gmail.com>
2023-12-18 22:07:06 -0300

    industries added

--- src/pages/industries.jsx
+++ src/pages/industries.jsx
@@ -1,23 +1,43 @@
 import React from "react";
-//import {industrieData} from "../data/index.js";
+import {industriesData} from "../data/index.js";
 
 const Industries = () => {
   return <>
   <section 
-    style={{
-      backgroundColor:'#000',
-      backgroundImage:'url("../../public/assets/images/bkg-industries.png")',
-      //backgroundSize:'cover',
-      //backgroundPosition:'left',
-      backgroundPositionX:'right',
-      minHeight:'100vh'
-    }}
+
     className="industries">
     {/* <img style={{width:'100vw', height:'100vh'}} src="../../public/assets/images/bkg-industries.png" alt="" /> */}
     <div className="container">
-      <div className="subtitle">
+      <div 
+      className="subtitle">
         <h2>Industries</h2>
         <p>Codefend is your comprehensive solution for all your company's security needs. Our application combines a wide range of services designed to protect your organization from cyber threats, ensuring the highest level of protection for your data & infrastructure.</p>
+      </div>
+      <div className="contain">
+
+
+        {
+          industriesData.map((data, i)=>{
+            return (
+              <div 
+                key={data.title} 
+                className="item">
+                <div 
+                style={{
+                  backgroundImage: `url(../../public/assets/images/item_${i + 1}.png)`
+                }}
+                className="image">
+                </div>
+                <div>
+                  <h3>{data.title}</h3>
+                  <p>{data.description}</p>
+                </div>
+              </div>
+            )
+          })
+        }
+
+
       </div>
     </div>
   </section>
