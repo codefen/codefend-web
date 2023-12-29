4463b209172ee23cfa2da650bba4d662d5e3b347
mixael442 <misaelcapo720@gmail.com>
2023-12-29 16:53:04 -0300

    menu responsive not finished

--- src/pages/software/features.jsx
+++ src/pages/software/features.jsx
@@ -25,10 +25,10 @@ const Features = () => {
                         software cuenta con un sistema de chat integrado que nos hace acordar a IRC</motion.p>
                     </div>
                     <motion.img
-                    initial={{y:'20vw', opacity:0}}
-                    whileInView={{y:0, opacity:1}}
-                    viewport={{once:true}}
-                    transition={{duration:1, delay:1.5}} 
+                    // initial={{y:'20vw', opacity:0}}
+                    // whileInView={{y:0, opacity:1}}
+                    // viewport={{once:true}}
+                    // transition={{duration:1, delay:1.5}} 
                     loading="lazy" 
                     src="../assets/images/app-black2.webp" 
                     alt="image of app" />
@@ -52,10 +52,10 @@ const Features = () => {
                         ><b>Unvenil your shadow IT with our automated attack surface expansion.</b>Descubra y conserve actualizado su superficie de ataque con nuestros sistemas de detección automatizado y manual</motion.p>
                     </div>
                     <motion.img
-                    initial={{y:'20vw', opacity:0}}
-                    whileInView={{y:0, opacity:1}}
-                    transition={{duration:1, delay:1.5}} 
-                    viewport={{once:true}}
+                    // initial={{y:'20vw', opacity:0}}
+                    // whileInView={{y:0, opacity:1}}
+                    // transition={{duration:1, delay:1.5}} 
+                    // viewport={{once:true}}
                     loading="lazy" 
                     src="../assets/images/app-black4.webp" 
                     alt="image of app" />
