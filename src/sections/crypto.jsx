60849e73b2d74ce622a3d5290f397dfeedc8a87b
ignacio gomez <ignaciob.gomez@gmail.com>
2024-01-04 22:11:36 -0300

    crypto

--- src/sections/crypto.jsx
+++ src/sections/crypto.jsx
@@ -1,29 +1,34 @@
+function Crypto() {
+  return (
+    <section className="crypto">
+      <div className="container">
+        <div className="coin-div">
+          <h3>bitcoin is welcome</h3>
 
-function Crypto(){
-    return (
-        <div 
-        className="crypto">
-        <div className="container">
-        <div className="coin">
-          <div
-            className="img-coin" >
-            <img className="coin1"  loading="lazy" src="../assets/images/coinred.webp" alt="bitcoin" />
-          </div>
-
-        <div>
-            <h3
-            >bitcoin is welcome</h3>        
+          <div className="content">
+            <div className="img-coin">
+              <img
+                className="coin1"
+                loading="lazy"
+                src="../assets/images/coinred.webp"
+                alt="bitcoin"
+              />
+            </div>
             <p>
-            <b><span>for customers and our ethical hackers:</span><br />
-              We accept all major cryptocurrencies, including bitcoin,
-              ethereum, and litecoin.</b> By accepting these 
-              cryptocurrencies, we are making it easier for
-              our customers to do business with us.</p>
-          </div>
+              <b>
+                <span>for customers and our ethical hackers:</span>
+                <br />
+                We accept all major cryptocurrencies, including bitcoin,
+                ethereum, and litecoin.
+              </b>{" "}
+              By accepting these cryptocurrencies, we are making it easier for
+              our customers to do business with us.
+            </p>
           </div>
         </div>
-        </div>
-    )
+      </div>
+    </section>
+  );
 }
 
-export default Crypto
+export default Crypto;
