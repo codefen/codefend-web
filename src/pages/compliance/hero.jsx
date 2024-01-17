3c311673a54d0e22ec9f7a1ee732467c145e0a99
ignacio gomez <ignaciob.gomez@gmail.com>
2024-01-17 20:21:45 -0300

    fix fix fix

--- src/pages/compliance/hero.jsx
+++ src/pages/compliance/hero.jsx
@@ -1,3 +1,5 @@
+import { complianceData } from "../../data"
+
 const HeroCompliance = () => {
     return (
         <section className="herocompliance">
@@ -11,6 +13,58 @@ const HeroCompliance = () => {
                         establishing direct communication between our professionals and your
                         security team.
                     </p>
+                </div>
+            </div>
+            <div className="container tarjetas">
+                <input type="text" placeholder="search" />
+                <div className="cards">
+                    <div className="card_container">
+                        {
+                            complianceData[0].map((data) => {
+                                return (
+                                    <div key={data.title} className="card">
+                                        <h3><b>{data.title}</b></h3>
+                                        <p>{data.description1}
+                                            <br />
+                                            <br />
+                                            {data.description2}
+                                        </p>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                    <div className="card_container">
+                        {
+                            complianceData[1].map((data) => {
+                                return (
+                                    <div key={data.title} className="card">
+                                        <h3><b>{data.title}</b></h3>
+                                        <p>{data.description1}
+                                            <br />
+                                            <br />
+                                            {data.description2}
+                                        </p>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div><div className="card_container">
+                        {
+                            complianceData[2].map((data) => {
+                                return (
+                                    <div key={data.title} className="card">
+                                        <h3><b>{data.title}</b></h3>
+                                        <p>{data.description1}
+                                            <br />
+                                            <br />
+                                            {data.description2}
+                                        </p>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
                 </div>
             </div>
         </section>
--- src/pages/compliance/index.jsx
+++ src/pages/compliance/index.jsx
@@ -1,12 +1,10 @@
 import React, { useState, useRef, useEffect } from 'react';
 import HeroCompliance from './hero';
-import Compliance1 from './compliance1';
 
 const Compliance = () => {
     return (
         <>
             <HeroCompliance />
-            <Compliance1 />
         </>
     );
 };
