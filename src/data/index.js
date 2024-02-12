bfe2fd00a80c7bffd706cef4ea5448f65d87a194
mixael442 <misaelcapo720@gmail.com>
2024-02-12 00:53:42 -0300

    matias and marcos added

--- src/data/index.js
+++ src/data/index.js
@@ -39,7 +39,7 @@ export const members = [
     {image:'../assets/images/members/miguel.png',name:'Miguel Peroni', rol:'Security & CyberSecurity', profile:`${link}peroni/`,experience:'8+ years'},
     {image:'../assets/images/members/zaballa.png',name:'Federico Zaballa', rol:'Head of team, Web 3.0', profile:`${link}zaballaf`,experience:'15+ years'},
     {image:'../assets/images/members/gaspar.png',name:'Gaspar O.', rol:'pentest', profile:`${link}gonesto/`,experience:'4+ years'},
-    {image:'../assets/images/members/matias.png',name:'Matias', rol:'software', profile:`${link}matias/`,experience:'3+ years'},
+    {image:'../assets/images/members/matias.png',name:'Matias', rol:'software', profile:`${link}matias/`,experience:'10+ years'},
     {image:'../assets/images/members/marcos.png',name:'Marcos', rol:'software', profile:`${link}marcos/`,experience:'3+ years'}
 ]
 
