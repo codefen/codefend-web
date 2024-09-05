d58910cda102bb3dc4501a6aecb1cad9eef76bf4
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-09-05 13:23:34 -0300

    chore: update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,27 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import LightningCSS from "unplugin-lightningcss/vite";
 
 export default defineConfig(() => {
   return {
+    base: "./",
+    publicDir: "public",
+    root: "./",
     build: {
-      outDir: 'build',
+      outDir: "build",
+      cssMinify: "lightningcss",
     },
-    plugins: [react()],
+    css: {
+      transformer: "lightningcss",
+      lightningcss: {
+        targets: { chrome: 115, firefox: 115, safari: 16 },
+      },
+    },
+    plugins: [react(), LightningCSS()],
+    clearScreen: false,
+    server: {
+      host: true,
+      port: process.env.PORT ? +process.env.PORT : 3000,
+    },
   };
 });
